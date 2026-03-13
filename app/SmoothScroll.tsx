48ba4de7abab1c19fe2ae4212a7e344d7b1776e6
jainamjyoat <jainamjyoat2005@gmail.com>
2026-03-13 23:24:13 +0530

    Fixx all the build issues

--- app/SmoothScroll.tsx
+++ app/SmoothScroll.tsx
@@ -14,7 +14,6 @@ export default function SmoothScroll() {
       duration: reducedMotion ? 0 : 1.05,
       easing: (t: number) => 1 - Math.pow(1 - t, 3), // cubic out
       smoothWheel: true,
-      normalizeWheel: true,
       wheelMultiplier: 1,
       touchMultiplier: 1,
       autoRaf: false, // we drive rAF manually to avoid duplicate loops
@@ -29,8 +28,7 @@ export default function SmoothScroll() {
 
     return () => {
       if (rafRef.current !== null) cancelAnimationFrame(rafRef.current);
-      // @ts-expect-error internal types allow destroy
-      lenis.destroy?.();
+      lenis.destroy();
     };
   }, []);
 
